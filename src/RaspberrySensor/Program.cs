2eedb198e8d2b1f3efc11ef4b9afda72f3ed0472
Richard Cupples <rcupples@outlook.com>
2020-03-14 14:22:31 +0100

    Initial Checkin

---
+++ src/RaspberrySensor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RaspberrySensor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
